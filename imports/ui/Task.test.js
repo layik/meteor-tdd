b3d116470dad04749e8bfb52d676768070f1169b
Layik Hama <layik@softcode.co.uk>
2018-01-16 12:40:04 +0000

    uncomment the shallow count children line

--- imports/ui/Task.test.js
+++ imports/ui/Task.test.js
@@ -1,5 +1,6 @@
 import { Meteor } from 'meteor/meteor';
 import { shallow } from 'enzyme';
+import { expect } from 'chai';
 import React from 'react';
 
 //move these to init test
@@ -23,7 +24,7 @@ describe('Tasks', () => {
   describe('Render', () => {
     it('There should be one li\'s', () => {
       const wrapper = shallow(<Task task={{text: "Hello"}}/>);
-      // expect(wrapper.children()).to.have.lengthOf(1)
+      expect(wrapper.children()).to.have.lengthOf(1)
     });
   })
 })
